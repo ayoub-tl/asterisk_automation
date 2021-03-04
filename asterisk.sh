bc46c747fa230a1bf5ec8f8b4bd262906fb7c11e
Ayoub Tellaa <68382823+ayoub-tl@users.noreply.github.com>
2021-03-04 21:54:52 +0100

    Update asterisk.sh

--- asterisk.sh
+++ asterisk.sh
@@ -2,74 +2,61 @@
 automation_file=$(pwd)
 apt update
 add-apt-repository universe
+#install dependicies
 apt -y install git curl wget libnewt-dev libedit-dev libssl-dev libncurses5-dev subversion libsqlite3-dev build-essential libjansson-dev libxml2-dev  uuid-dev
 cd ~
 wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-18-current.tar.gz
 tar xvf asterisk-18-current.tar.gz
 file=$(ls  | grep asterisk.* | grep -v '.tar.gz')
 cd $file
-
+#load the configuration of the machine
 ./configure --with-jansson-bundled
-
+#compile the configuration
 make
-
+#install
 make install
-
+#load the  /etc/asterisk with sample config
 make samples
-
 make config
 
 cd ~/asterisk-18.2.1/contrib/init.d
-
+#creat a inint.d/asterisk file to launch the service
 cp rc.debian.asterisk /etc/init.d/asterisk
-
+#REPLACE THE PLACEHOLDER IN THE CONFIG
 BFILE=$(type asterisk | awk '{ print $3 }' )
 sed -i "s|__ASTERISK_SBIN_DIR__\/asterisk|$BFILE|g" /etc/init.d/asterisk
-
 sed -i "s/__ASTERISK_VARRUN_DIR__/\/var\/lib\/asterisk/g" /etc/init.d/asterisk
-
 sed -i "s/__ASTERISK_ETC_DIR__/\/etc\/asterisk/g" /etc/init.d/asterisk
-
+#change asterick to run as user asterisk
 useradd -d /var/lib/asterisk  asterisk
 chown -R asterisk  /var/spool/asterisk /var/lib/asterisk /var/run/asterisk
 
+
 cd ~/$file/contrib/init.d/
-
-
 cp etc_default_asterisk /etc/defualt/asterisk
-
+#uncmommnet the user field
 sed -i "s/;AST_USER/AST_USER/g" /etc/default/asterisk
 sed -i "s/;AST_GROUP/AST_USER/g" /etc/default/asterisk
-
 update-rc.d asterisk defaults
-#change the network to static
-# /etc/netplan/00-installer-config.yaml
-#******
-#//
-# file=$(ls /etc/netplan/)
-# cat /etc/netplan/$file | grep -n dhcp | awk '{print $1}' | cut -c 1
-# sed "s/dhcp4: true/dhcp4: false/"
-# a$
-#netplan apply
 
 
+
+#install dhcp
 apt get install dhcpd
-#vi /etc/default/udhcpd=> dhcp_enabled =yes
+
 sed -i "s/DHCP_ENABLED=\"no\"/DHCP_ENABLED=\"yes\"/g"
 
-#vi /etc/udhcpd.conf =>range
-#*****
+
 sed -i 's/.*start.*/start       192.168.1.40 /g' /etc/udhcpd.conf
 sed -i 's/.*start.*/start       192.168.1.49 /g' /etc/udhcpd.conf
-#**dns
+#configure dns & getway
 sed -i 's/opt  dns.*/opt   dns     8.8.8.8/g' /etc/udhcpd.conf
 sed -i 's/opt  router.*/opt   router     192.168.1.1/g' /etc/udhcpd.conf
 sed -i '/option     dns/d' /etc/udhcpd.conf
 sed -i '/option     wins/d' /etc/udhcpd.conf
 
-
+#instlall ntp
 apt install -y ntp 
-
 /etc/init.d/ntp start 
 
 
@@ -84,38 +71,17 @@ sed '/^$/d' sip.conf
 #add qualify=yes to genral cintext
 sed -i '9iqualify=yes' sip.conf
 
+#copy the document +empty the config file
 cp extensions.conf extensions.conf.copy
 echo '' > extensions.conf
 
-
+#create user and alias aka extension
 $automation_file/create_sip_user.sh user1 password2 alias1
 $automation_file./create_sip_user.sh user2 password2 alais2
 
 
 
 
-
-# cat <<EOF>> test
-
-# [user]
-# type=friend
-# context=ulaw,alaw
-# secret=12345678
-# host=dynamic
-
-# EOF
-
-
-
-#done in create user
-# cat <<EOF>> test
-
-# [phones]
-# exten =>100,1,NoOp('first dial')
-# same => n,Dial(SIP/peer)
-# same => n,Hangup
-# EOF
-
-/etc/init.d/asterisk stop
 /etc/init.d/asterisk reload
+/etc/init.d/asterisk stop
 /etc/init.d/asterisk start
